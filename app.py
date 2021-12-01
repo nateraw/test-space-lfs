ebe2710f0db2778ed91e8b022dc47f20915a6fca
nateraw <nxr9266@g.rit.edu>
2021-11-30 22:08:28 -0500

    :bug: allow html

--- app.py
+++ app.py
@@ -9,13 +9,4 @@ text = Path('README.md').read_text()
 match = REGEX_YAML_BLOCK.search(text)
 page_content = match.group(2)
 
-st.markdown(page_content)
-# st.markdown(
-#     """
-#     <p align="center">
-#         <a href="https://github.com/nateraw/huggingpics-explorer" alt="Repo"><img src="https://img.shields.io/github/stars/nateraw/huggingpics-explorer?style=social" /></a>
-#     </p>
-#     """,
-#     unsafe_allow_html=True,
-# )
-# st.write('Some change!')
+st.markdown(page_content, unsafe_allow_html=True)
